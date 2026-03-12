35d8587465825156b44488bd7ef2edc28d58df8b
s34264 <s34264@pjwstk.edu.pl>
2026-03-12 13:34:50 +0100

    Dodano funkcję 'start' uruchamiającą Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Calculator {
 
     public int sum(int a, int b){
@@ -6,7 +8,7 @@ public class Calculator {
     public int sub(int a, int b){
         return a-b;
     }
-    public int multi(int a, int b){
+    public int mul(int a, int b){
         return a*b;
     }
     public int div(int a, int b){
@@ -21,5 +23,36 @@ public class Calculator {
             base*=base;
         }
         return base;
+    }
+
+    public void start(){
+        Scanner input = new Scanner(System.in);
+        String userdata = "";
+        userdata = input.nextLine();
+        while(!userdata.equals("exit")){
+            int a = Integer.parseInt( userdata.split(" ")[0]);
+            String op = userdata.split(" ")[1];
+            int b = Integer.parseInt( userdata.split(" ")[2]);
+
+            int res = -1;
+
+            switch(op){
+                case "+":
+                    System.out.println(sum(a,b));
+                    break;
+                case "-":
+                    System.out.println(sub(a,b));
+                    break;
+                case "*":
+                    System.out.println(mul(a,b));
+                    break;
+                default:
+                    System.out.println("error");
+                    break;
+            }
+            userdata = input.nextLine();
+        }
+
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println(calc.sum(2,3));
+        calc.start();
     }
 }
