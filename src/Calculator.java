ce6fa5cb222059b6ca66c6030b0a462ff54b171c
s34264 <s34264@pjwstk.edu.pl>
2026-03-12 13:18:35 +0100

    test 2

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public int sum(int a, int b){
+        return a+b;
+    }
+    public int sub(int a, int b){
+        return a-b;
+    }
+    public int mul(int a, int b){
+        return a*b;
+    }
+    public int div(int a, int b){
+        return a/b;
+    }
+    public int mod(int a, int b){
+        //comment
+        return a%b;
+    }
+}
