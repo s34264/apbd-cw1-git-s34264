1621314fbd66d6d8242eb2ce6337ea5b0aac898b
Zofia Michalska <s34264@pjwstk.edu.pl>
2026-03-19 22:51:40 +0100

    W klasie Calculator metodzie sum() zmieniono nazwę zmiennej "sum" na "result"

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,8 @@
 public class Calculator {
 
     public void sum(int a, int b) {
-        int sum = a + b;
-        System.out.println(sum);
+        int result = a + b;
+        System.out.println(result);
     }
 
     public void sumArray(int[] tab){
