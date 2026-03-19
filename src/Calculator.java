8393e5c0008af8e6efc9e23f8be46661a1279469
Zofia Michalska <s34264@pjwstk.edu.pl>
2026-03-19 18:42:54 +0100

    W klasie Calculator zdefiniowano metodę calculateAverage().

--- src/Calculator.java
+++ src/Calculator.java
@@ -38,6 +38,12 @@ public class Calculator {
     }
 
     public void calculateMin(int[] nums){
-
+        int min = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            if(nums[i] < min){
+                min = nums[i];
+            }
+        }
+        System.out.println(min);
     }
 }
