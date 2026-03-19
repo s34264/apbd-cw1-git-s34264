b43e5a382d5d1f91196a7c0a2fa518e695dbb2b1
Zofia Michalska <s34264@pjwstk.edu.pl>
2026-03-19 18:14:17 +0100

    Wszystkie metody klasy Calculator teraz wyświetlają wynik.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,17 +1,22 @@
 public class Calculator {
+
     public void sum(int a, int b) {
         int sum = a + b;
+        System.out.println(sum);
     }
 
     public void sub(int a, int b) {
         int sub = a - b;
+        System.out.println(sub);
     }
 
     public void mul(int a, int b) {
         int mul = a * b;
+        System.out.println(mul);
     }
 
     public void div(int a, int b) {
         int div = a / b;
+        System.out.println(div);
     }
 }
