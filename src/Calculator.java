3db70bbec295a6ecbde843f44826e9cf47da6334
Zofia Michalska <s34264@pjwstk.edu.pl>
2026-03-19 18:12:40 +0100

    Rozszerzono klasę Calculator o metody mul() oraz div()

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,4 +6,12 @@ public class Calculator {
     public void sub(int a, int b) {
         int sub = a - b;
     }
+
+    public void mul(int a, int b) {
+        int mul = a * b;
+    }
+
+    public void div(int a, int b) {
+        int div = a / b;
+    }
 }
